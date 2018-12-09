868c616c1824604068d89c436fa61f88fc623deb
khaled Hamoul <fk_hamoul@esi.dz>
2018-12-09 06:21:24 +0100

    add the hole project (begining)

---
+++ app/js/vue-config.js
@@ -0,0 +1,9 @@
+import 'vue.js';
+// Components
+// Vue.component('component-name', require('./components/component.vue'));
+Vue.component('info', require('./components/info.vue'));
+
+// Vue instance
+const app = new Vue({
+    el: '#vue-div',
+});
